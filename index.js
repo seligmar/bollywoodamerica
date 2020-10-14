8122cfed08bb03a4ad135d966e1de3b0b7fd1211
github username <github email address>
2020-10-14 20:09:29 +0100

    cleaned up Jab We Met text

--- index.js
+++ index.js
@@ -53,12 +53,12 @@ document.addEventListener('keydown', function (e) {
   var requiredKey = konamiCode[konamiCodePosition]
 
   // compare the key with the required key
-  if (key == requiredKey) {
+  if (key === requiredKey) {
     // move to the next key in the konami code sequence
     konamiCodePosition++
 
     // if the last key is reached, activate cheats
-    if (konamiCodePosition == konamiCode.length) {
+    if (konamiCodePosition === konamiCode.length) {
       activateCheats()
       konamiCodePosition = 0
     }
